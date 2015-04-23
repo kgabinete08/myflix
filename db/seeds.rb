626926a434c5b517b00ce5fb9bb13524f28b6c2a
Kevin Gabinete <kgabinete08@gmail.com>
2015-04-22 18:45:07 -0700

    created video mvc and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: 'Family Guy', description: 'Brian makes Stewie watch viral video.', small_cover_url: '/tmp/family_guy.jpg')
+Video.create(title: 'Futurama', description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation.', small_cover_url: '/tmp/futurama.jpg')
