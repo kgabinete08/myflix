195791643f0f6d8157468031fa77d8642d22c140
Kevin Gabinete <kgabinete08@gmail.com>
2015-04-28 12:14:41 -0700

    cosmetic changes, ui route added

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,23 @@
 comedy = Category.create!(name: 'Comedy')
 drama = Category.create!(name: 'Drama')
 
-Video.create!(title: 'Family Guy', description: 'Brian makes Stewie watch viral video.', small_cover_url: '/tmp/family_guy.jpg', category: comedy)
-Video.create!(title: 'Futurama', description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation.', small_cover_url: '/tmp/futurama.jpg', category: comedy)
-Video.create!(title: 'South Park', description: 'Follow the misadventures of four crazy grade-schoolers in the dysfunctional town of South Park, Colorado.', small_cover_url: '/tmp/south_park.jpg', category: comedy)
-Video.create!(title: 'Monk', description: "Brilliant but emotionally hampered former San Francisco police detective Adrian Monk works as a consultant on the SFPD's toughest cases.",
-              small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg', category: drama)
+Video.create!(title: 'Family Guy',
+              description: 'Brian makes Stewie watch viral video.',
+              small_cover_url: '/tmp/family_guy.jpg',
+              category: comedy)
+
+Video.create!(title: 'Futurama',
+              description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation.',
+              small_cover_url: '/tmp/futurama.jpg',
+              category: comedy)
+
+Video.create!(title: 'South Park',
+              description: 'Follow the misadventures of four crazy grade-schoolers in the dysfunctional town of South Park, Colorado.',
+              small_cover_url: '/tmp/south_park.jpg',
+              category: comedy)
+
+Video.create!(title: 'Monk',
+              description: "Brilliant but emotionally hampered former San Francisco police detective Adrian Monk works as a consultant on the SFPD's toughest cases.",
+              small_cover_url: '/tmp/monk.jpg',
+              large_cover_url: '/tmp/monk_large.jpg',
+              category: drama)
