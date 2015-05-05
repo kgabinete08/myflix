ce05a2d708e7d23b5ef8c449b1bfe50cf0095b94
Kevin Gabinete <kgabinete08@gmail.com>
2015-05-04 21:08:23 -0700

    user authentication and flash messages formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      flash[:notice] = "Your account was created successfully."
       redirect_to sign_in_path
     else
       render :new
