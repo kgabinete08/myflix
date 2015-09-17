651a2a373cba10e8a00c2a1a7a13e229fb14e486
Kevin Gabinete <kgabinete08@gmail.com>
2015-09-16 21:54:01 -0700

    people page implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 
   has_many :queue_items, -> { order(:position) }
   has_many :reviews, -> { order("created_at DESC") }
+  has_many :following_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :leading_relationships, class_name: "Relationship", foreign_key: :leader_id
 
   def queued_video?(video)
     self.queue_items.map(&:video).include?(video)
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Myflix::Application.routes.draw do
   get '/register', to: 'users#new'
   get 'sign_in', to: 'sessions#new'
   get '/sign_out', to: 'sessions#destroy'
-  get 'my_queue', to: 'queue_items#index'
 
   resources :videos, only: [:show] do
     collection do
@@ -14,10 +13,13 @@ Myflix::Application.routes.draw do
     resources :reviews, only: [:create]
   end
 
-  resources :categories, only: [:show]
   resources :users, only:[:create, :show]
+  resources :relationships, only: [:destroy]
+  get 'people', to: 'relationships#index'
+
+  resources :categories, only: [:show]
   resources :sessions, only: [:create]
   resources :queue_items, only: [:create, :destroy]
+  get 'my_queue', to: 'queue_items#index'
   post 'update_queue', to: 'queue_items#update_queue'
-
 end
