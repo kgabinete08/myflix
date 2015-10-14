e28ac760bc72c52a582d9b7b87e953ec406b95a0
Kevin Gabinete <kgabinete08@gmail.com>
2015-10-13 21:10:18 -0700

    add video tests

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -4,4 +4,8 @@ class Admin::VideosController < ApplicationController
   def new
     @video = Video.new
   end
+
+  def create
+
+  end
 end
--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -6,10 +6,8 @@ describe Admin::VideosController do
       let(:action) { get :new }
     end
 
-    it "sets @video to a new video" do
-      set_current_admin
-      get :new
-      expect(assigns(:video)).to be_a_new Video
+    it_behaves_like "requires admin" do
+      let(:action) { get :new }
     end
 
     it "redirects non admin users to the home path" do
@@ -22,6 +20,29 @@ describe Admin::VideosController do
       set_current_user
       get :new
       expect(flash[:danger]).to be_present
+    end
+  end
+
+  describe "POST create" do
+    it_behaves_like "requires sign in" do
+      let(:action) { post :create }
+    end
+
+    it_behaves_like "requires admin" do
+      let(:action) { post :create }
+    end
+
+    context "with valid input" do
+      it "creates the video"
+      it "redirects to the add new video pagee"
+      it "sets the flash success message"
+    end
+
+    context "with invalid input" do
+      it "does not create a video"
+      it "renders the new template"
+      it "sets @video"
+      it "sets the flash danger message"
     end
   end
 end
