11783bc5b2a953e1f30c776cb51b3abc22cfd406
Kevin Gabinete <kgabinete08@gmail.com>
2015-10-14 21:04:55 -0700

    admin video create action implemented

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,6 +6,19 @@ class Admin::VideosController < ApplicationController
   end
 
   def create
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:success] = "Your video has been added."
+      redirect_to new_admin_video_path
+    else
+      flash.now[:danger] = "There were some errors adding your video."
+      render :new
+    end
+  end
 
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :description, :category_id)
   end
 end
--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -33,16 +33,56 @@ describe Admin::VideosController do
     end
 
     context "with valid input" do
-      it "creates the video"
-      it "redirects to the add new video pagee"
-      it "sets the flash success message"
+      it "redirects to the add new video page" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: "Ghostbusters", description: "Who you gonna call?", category_id: category.id }
+        expect(response).to redirect_to new_admin_video_path
+      end
+
+      it "creates the video" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: "Ghostbusters", description: "Who you gonna call?", category_id: category.id }
+        expect(category.videos.count).to eq(1)
+      end
+
+      it "sets the flash success message" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: "Ghostbusters", description: "Who you gonna call?", category_id: category.id }
+        expect(flash[:success]).to be_present
+      end
     end
 
     context "with invalid input" do
-      it "does not create a video"
-      it "renders the new template"
-      it "sets @video"
-      it "sets the flash danger message"
+      it "does not create a video" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: "Ghostbusters", category_id: category.id }
+        expect(category.videos.count).to eq(0)
+      end
+
+      it "renders the new template" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: "Ghostbusters", category_id: category.id }
+        expect(response).to render_template :new
+      end
+
+      it "sets @video" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: "Ghostbusters", category_id: category.id }
+        expect(assigns(:video)).to be_present
+      end
+
+      it "sets the flash danger message" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: "Ghostbusters", category_id: category.id }
+        expect(flash[:danger]).to be_present
+      end
     end
   end
 end
