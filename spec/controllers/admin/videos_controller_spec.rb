2791ea9bca5e839eb91df9d20762cfd9bfd0a291
Kevin Gabinete <kgabinete08@gmail.com>
2015-10-28 12:48:33 -0700

    refactored admin video controller spec

--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -33,53 +33,45 @@ describe Admin::VideosController do
     end
 
     context "with valid input" do
+      before { set_current_admin }
+      let(:category) { Fabricate(:category) }
+
       it "redirects to the add new video page" do
-        set_current_admin
-        category = Fabricate(:category)
         post :create, video: { title: "Ghostbusters", description: "Who you gonna call?", category_id: category.id }
         expect(response).to redirect_to new_admin_video_path
       end
 
       it "creates the video" do
-        set_current_admin
-        category = Fabricate(:category)
         post :create, video: { title: "Ghostbusters", description: "Who you gonna call?", category_id: category.id }
         expect(category.videos.count).to eq(1)
       end
 
       it "sets the flash success message" do
-        set_current_admin
-        category = Fabricate(:category)
         post :create, video: { title: "Ghostbusters", description: "Who you gonna call?", category_id: category.id }
         expect(flash[:success]).to be_present
       end
     end
 
     context "with invalid input" do
+      before { set_current_admin }
+      let(:category) { Fabricate(:category) }
+
       it "does not create a video" do
-        set_current_admin
-        category = Fabricate(:category)
         post :create, video: { title: "Ghostbusters", category_id: category.id }
         expect(category.videos.count).to eq(0)
       end
 
       it "renders the new template" do
-        set_current_admin
-        category = Fabricate(:category)
         post :create, video: { title: "Ghostbusters", category_id: category.id }
         expect(response).to render_template :new
       end
 
       it "sets @video" do
-        set_current_admin
-        category = Fabricate(:category)
         post :create, video: { title: "Ghostbusters", category_id: category.id }
         expect(assigns(:video)).to be_present
       end
 
       it "sets the flash danger message" do
-        set_current_admin
-        category = Fabricate(:category)
         post :create, video: { title: "Ghostbusters", category_id: category.id }
         expect(flash[:danger]).to be_present
       end
